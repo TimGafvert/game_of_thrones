f9bd14d84fc8a3567409a8f643140f698e30766c
Admin <Tgafvert@gmail.com>
2017-08-03 11:02:44 -0400

    basic but need D

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,44 @@
+class CharactersController < ApplicationController
+
+  def index
+    @house = House.find(params[:house_id])
+    @characters = @house.characters.all
+  end
+
+  def show
+    @character = Character.find(params[:id])
+  end
+
+  def new
+    @house = House.find(params[:house_id])
+    @character = Character.new
+  end
+
+  def create
+    @house = House.find(params[:house_id])
+    @character = @house.characters.create!(character_params)
+    redirect_to house_character_path(@house, @character)
+  end
+
+  def edit
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+    redirect_to house_character_path(@house, @character)
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to house_character_path(@house, @character)
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+
+    root "houses#index"
+    resources :houses do
+      resources :characters
+    end
+
+
+
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
